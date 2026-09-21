4094165817051d5f2cede8de5edc3379069e5297
Aarya Deshmukh <aaryadeshmukh01@gmail.com>
2026-09-21 11:33:06 +0530

    WGHO

--- SQL.sql
+++ SQL.sql
@@ -1,87 +1,48 @@
-use t388;
-show tables;
+use T388;
+CREATE TABLE Projects (
+ProjectId INT PRIMARY KEY AUTO_INCREMENT, 
+ProjectName VARCHAR(200) NOT NULL,
+EmployeeId INT,  
+StartDate DATETIME,  
+EndDate DATETIME);
+INSERT INTO Projects VALUES
+(1,'Develop Ecommerse Website from
+scratch', 1003, NOW(), DATE_ADD(NOW(), INTERVAL 30 DAY)),
+(2,'WordPress Website for our company',
+1002, NOW(), DATE_ADD(NOW(), INTERVAL 45 DAY)),
+(3,'Manage our Company Servers', 1007,
+NOW(), DATE_ADD(NOW(), INTERVAL 45 DAY)),
+(4,'Hosting account is not working', 1009,
+NOW(), DATE_ADD(NOW(), INTERVAL 7 DAY)),
+(5,'MySQL database from my desktop
+application', 1010, NOW(), DATE_ADD(NOW(), INTERVAL 15 DAY)),
+(6,'Develop new WordPress plugin for my business
+website', NULL, NOW(), DATE_ADD(NOW(), INTERVAL 10 DAY)),
+(7,'Migrate web application and database to
+new server', NULL, NOW(), DATE_ADD(NOW(), INTERVAL 5 DAY)),
+(8,'Android Application development', 1004,
+NOW(), DATE_ADD(NOW(), INTERVAL 30 DAY)),
+(9,'Hosting account is not working', 1001,
+NOW(), DATE_ADD(NOW(), INTERVAL 7 DAY)),
+(10,'MySQL database from my desktop
+application', 1008, NOW(), DATE_ADD(NOW(), INTERVAL 15 DAY)),
+(11,'Develop new WordPress plugin for my
+business website', NULL, NOW(), DATE_ADD(NOW(), INTERVAL 10 DAY));
+select *,datediff(enddate, startdate)as Duration from Projects;
+select ProjectName from Projects where datediff(enddate, startdate)= '45';
+alter table projects add duration int;
+select * from projects;
+update projects set duration=datediff(enddate, startdate);
+select ProjectName from Projects where duration= 45;
+select duration,count(*) from projects group by duration;
+select duration,count(*) from projects group by duration having count(*)>=2;
+select duration,count(*) from projects 
+where duration>=20
+group by duration 
+having count(*)>=2
+order by duration desc;
+
+-- W G H O -- where -> group by -> having -> order by
+
 select * from employee;
-delete from employee where Gender="male";
-delete from employee where age>25;
-INSERT INTO Employee values
-(1001,"John Doe","IT",35000,"Male",25),
-(1002, 'Mary Smith', 'HR', 45000, 'Female', 27),
-(1003, 'James Brown', 'Finance', 50000, 'Male', 28),
-(1004, 'Mike Walker', 'Finance', 50000, 'Male', 28),
-(1005, 'Linda Jones', 'HR', 75000, 'Female', 26),
-(1006, 'Anurag Mohanty', 'IT', 35000, 'Male', 25),
-(1007, 'Priyanka Dewangan', 'HR', 45000, 'Female', 27),
-(1008, 'Sambit Mohanty', 'IT', 50000, 'Male', 28),
-(1009, 'Pranaya Kumar', 'IT', 50000, 'Male', 28),
-(1010, 'Hina Sharma', 'HR', 75000, 'Female', 26);
-truncate table employee;
-alter table employee add location varchar(10);
-select * from employee;
-desc employee;
-alter table employee modify FullName varchar(40);
-alter table employee add bonus float after salary;
-alter table employee add Title varchar(5) first;
-alter table employee modify FullName varchar(35);
-alter table employee change column location Address varchar(36);
-update employee set address="Thane";
-alter table employee drop title, drop bonus, drop address;
-INSERT INTO Employee values
-(1001,"John Doe","IT",35000,"Male",25),
-(1002, 'Mary Smith', 'HR', 45000, 'Female', 27),
-(1003, 'James Brown', 'Finance', 50000, 'Male', 28),
-(1004, 'Mike Walker', 'Finance', 50000, 'Male', 28),
-(1005, 'Linda Jones', 'HR', 75000, 'Female', 26),
-(1006, 'Anurag Mohanty', 'IT', 35000, 'Male', 25),
-(1007, 'Priyanka Dewangan', 'HR', 45000, 'Female', 27),
-(1008, 'Sambit Mohanty', 'IT', 50000, 'Male', 28),
-(1009, 'Pranaya Kumar', 'IT', 50000, 'Male', 28),
-(1010, 'Hina Sharma', 'HR', 75000, 'Female', 26);
-select * from employee;
-alter table employee add location varchar(10);
-alter table employee add bonus float after salary;
-alter table employee add Title varchar(5) first;
-alter table employee change column location Address varchar(36);
-update employee set address="Thane";
-update employee set address="Dombivli" where department="IT";
-update employee set Title="Mr." where gender="Male";
-update employee set Title="Ms." where gender="Female";
-update employee set bonus=salary*0.05;
-select * from employee;
-
-
-
-use t388;
-create table Kisan_Info
-(ID int unique not null,
-name varchar(50) unique not null,
-age int check (age>=18),
-email_ID varchar(40) default "dummy123@gmail.com"
-);
-desc Kisan_Info;
-insert into Kisan_Info values
-(1001,"Jayant Kumar",59,"");
-select * from Kisan_Info;
-update Kisan_Info set email_ID=default
-where ID=1001;
-insert into Kisan_Info values
-(1002,"Rajesh Pradhan",56,default);
-alter table Kisan_Info modify age int check (age>=0);
-insert into Kisan_info values
-(1003,"kkkkkk",16,default);
-show create table Kisan_Info;
-alter table Kisan_Info drop check Kisan_Info_chk_1;
-alter table Kisan_Info drop check Kisan_Info_chk_2;
-alter table Kisan_Info drop check Kisan_Info_chk_3;
-insert into kisan_info (ID,name,age) values
-(1004,"hediuwheiduhwi",38);
-
-
-
-select distinct Department from employee;
-select distinct gender from employee;
-use t388;
-show tables;
-select * from employee
-where department="IT" or department="finance";
-select * from employee
-where department in ("HR","Finance");
+select * from employee limit 3,5;
